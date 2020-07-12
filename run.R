173cfc8ca7c2a48fc9e5774d1e70793250e6be46
ArijitRoy75 <arijitroy75@gmail.com>
2020-07-12 20:50:33 +0530

    file rename

--- run.R
+++ run.R
@@ -1,14 +1,14 @@
-# my_packages = c("plyr","shiny","plotly","shinythemes","shinyWidgets","shinydashboard","lubridate","prophet","DT")
-# 
-# install_if_missing = function(p) {
-#     if (p %in% rownames(installed.packages()) == FALSE) {
-#         install.packages(p, dependencies = TRUE)
-#     }
-#     else {
-#         cat(paste("Skipping already installed package:", p, "\n"))
-#     }
-# }
-# invisible(sapply(my_packages, install_if_missing))
+my_packages = c("plyr","shiny","plotly","shinythemes","shinyWidgets","shinydashboard","lubridate","prophet","DT")
+
+install_if_missing = function(p) {
+    if (p %in% rownames(installed.packages()) == FALSE) {
+        install.packages(p, dependencies = TRUE)
+    }
+    else {
+        cat(paste("Skipping already installed package:", p, "\n"))
+    }
+}
+invisible(sapply(my_packages, install_if_missing))
 
 
 library(shiny)
