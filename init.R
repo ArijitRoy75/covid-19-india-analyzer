b7dd690622448a141885b621842d990abbb73d45
ArijitRoy75 <arijitroy75@gmail.com>
2020-07-14 02:16:31 +0530

    run

--- init.R
+++ init.R
@@ -1,5 +1,6 @@
 my_packages = c("drat","e1071","shiny","plotly","shinythemes","shinyWidgets","shinydashboard","lubridate","prophet","DT")
 
+install.packages("devtools")
 devtools::install_url("https://cran.r-project.org/src/contrib/Archive/BH/BH_1.62.0-1.tar.gz")
 install.packages('rstan')
 
