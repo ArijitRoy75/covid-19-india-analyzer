d0f2c85489f9df9a7ae1b38c0cff92cb912bc878
ArijitRoy75 <arijitroy75@gmail.com>
2020-07-14 03:35:39 +0530

    runapp

--- init.R
+++ init.R
@@ -1,8 +1,5 @@
 my_packages = c("drat","e1071","shiny","plotly","shinythemes","shinyWidgets","shinydashboard","lubridate","prophet","DT")
 
-install.packages("devtools")
-devtools::install_url("https://cran.r-project.org/src/contrib/Archive/BH/BH_1.62.0-1.tar.gz")
-install.packages('rstan')
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
