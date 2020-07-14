13d328b0cc28f4c8549cb93eaab4c7bc45c2dd38
ArijitRoy75 <arijitroy75@gmail.com>
2020-07-14 11:41:37 +0530

    run

--- init.R
+++ init.R
@@ -1,6 +1,6 @@
 my_packages = c("drat","rtools","e1071","shiny","plotly","shinythemes","shinyWidgets","shinydashboard","lubridate","prophet","DT")
 
-install.packages("rstan", type = "binary", dependencies = TRUE, repos = "https://cloud.r-project.org")
+install.packages("rtools", type = "source")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
